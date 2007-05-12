71095c2bde523e9064a4a853a1d5fb76671d2da6
lcflores <lcflores@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-05-12 22:16:27 +0000

    Adding first version of creative-commons-license-widget by lcflores
    git-svn-id: https://plugins.svn.wordpress.org/creative-commons-license-widget/tags/0.4@12729 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ccLicense.php
@@ -0,0 +1,85 @@
+<?php
+/****************************************************************************
+* Plugin Name: creative commons license widget
+* File: ccLicense.php
+* Description: Adds a sidebar widget to display creative commons license (Spanish / English)
+* Author: Lcflores
+* Date: 11/5/2007
+* Lenguage: php - widget  for wordpress 2.0 or higer.
+* Version: 0.4
+* Author URI: http://www.xperimentos.com
+****************************************************************************/
+
+// This gets called at the plugins_loaded action
+function widget_ccLicense_init() {
+	
+	// check for sidebar existance
+	if ( !function_exists('register_sidebar_widget') || !function_exists('register_widget_control') )
+		return;
+
+	// This saves options and prints the widget's config form.
+	function widget_ccLicense_control() {
+		$options = $newoptions = get_option('widget_ccLicense');
+		if ( $_POST['ccLicense-submit'] ) {
+			$newoptions['title'] = strip_tags(stripslashes($_POST['ccLicense-title']));
+			$newoptions['language'] = strip_tags(stripslashes($_POST['ccLicense-language']));      
+		}
+		if ( $options != $newoptions ) {
+			$options = $newoptions;
+			update_option('widget_ccLicense', $options);
+		}
+	?>
+				<p style="text-align:left;">
+				<label for="ccLicense-title" style="line-height:25px;display:block;"><?php _e('Widget title:', 'widgets'); ?> 
+          <input type="text" id="ccLicense-title" name="ccLicense-title" value="<?php echo wp_specialchars($options['title'], true); ?>" />
+				<label for="ccLicense-language" style="line-height:25px;display:block;"><?php _e('Widget Language:', 'widgets'); ?> 
+          <select id="ccLicense-language" name="ccLicense-lenguage">
+            <option><?php echo wp_specialchars($options['language'], true); ?></option>
+            <option value="Spanish">Spanish</option>
+            <option value="English">English</option>
+					</select>
+				</label>
+				<input type="hidden" name="ccLicense-submit" id="ccLicense-submit" value="1" />        
+				</p>
+	<?php
+	}    
+  
+  	// This prints the widget
+	function widget_ccLicense($args) {
+		extract($args);
+		$options = get_option('widget_ccLicense');
+		$title = $options['title'];
+    $language = $options['language'];
+    
+		echo $before_widget . $before_title . $title . $after_title;
+    if ($language eq "Spanish") {
+  		?>
+        <!--Creative Commons License-->
+          <a rel="license" href="http://creativecommons.org/licenses/by/3.0/deed.es_CL">
+          <img alt="Creative Commons License" border="0" src="http://creativecommons.org/images/public/somerights20.png"/></a>
+          <br/>
+          Blog bajo licencia <a rel="license" href="http://creativecommons.org/licenses/by/3.0/deed.es_CL">Creative Commons Attribution 3.0 License</a>. 
+        <!--/Creative Commons License-->
+  		<?php
+    }
+    else {
+      ?>
+      <!--Creative Commons License-->
+        <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">
+        <img alt="Creative Commons License" border="0" src="http://creativecommons.org/images/public/somerights20.png"/></a>
+        <br/>
+        Blog under the <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 License</a>. 
+      <!--/Creative Commons License-->
+      <?php    
+    }
+		echo $after_widget;
+	}
+  
+	// Tell Dynamic Sidebar about our new widget and its control
+	register_sidebar_widget(array('creativecommons License', 'widgets'), 'widget_ccLicense');
+	register_widget_control(array('creativecommons License', 'widgets'), 'widget_ccLicense_control');
+}
+
+// Delay plugin execution to ensure Dynamic Sidebar has a chance to load first
+add_action('widgets_init', 'widget_ccLicense_init');
+?>
---
+++ readme.txt
@@ -0,0 +1,30 @@
+=== creative commons license widget ===
+
+Plugin Name: creative commons license widget
+Contributors: lcflores
+Donate link: http://www.xperimentos.com/2007/05/11/creative-commons-license-widget-for-wordpress/
+Author URI: http://www.xperimentos.com
+Tags: license, creative commons
+Requires at least: 2.0.2
+Tested up to: 2.1
+Stable tag: 0.4
+Version: 0.4
+Creation time: 12/5/2007
+Last updated: 12/5/2007
+
+Adds a sidebar widget to display creative commons license (both Spanish and English languages are supported).
+
+
+== Description ==
+
+Adds a sidebar widget to display creative commons license.
+	Spanish and English languages are supported).
+
+
+== Installation ==
+
+1. Firstly, make sure you have the Widget plugin available at http://www.xperimentos.com/public/ccLicense.zip
+2. Copy "ccLicense.php" to your plugins folder, "/wp-content/plugins/widgets/"
+3. Click the "Activate" link for "creative commons license widget" on your Plugins page (in the WordPress admin interface)
+4. Go to Themes->Sidebar Widgets and drag and drop the widget to wherever you want to show it.
+5. Use the configuration menu to customise the widget.
